fa5c60f890535e925295f5e7a319a33d255f08ab
hoanglv <hoanglv.30@gmail.com>
2021-11-03 22:12:07 +0700

    74595 xuat 1 byte

---
+++ NODE/pic/pic16f887/xuatbyte74595.c
@@ -0,0 +1,18 @@
+
+#DEFINE      DS             PIN_D1
+#DEFINE      SH_CP             PIN_D0  
+#DEFINE      STCP             PIN_D2
+VOID XUATTRANGTHAI(UNSIGNED INT8 BYTEXUAT)
+{
+   UNSIGNED INT8   SB;   
+   #BIT BSERI  = BYTEXUAT.7
+   
+   FOR (SB=0;SB<8;SB++)
+      {                                                 
+         OUTPUT_BIT(DS,BSERI);    
+         OUTPUT_LOW(SH_CP); OUTPUT_HIGH(SH_CP);
+         BYTEXUAT=BYTEXUAT<<1; //10010010->00100100  
+     }
+      OUTPUT_LOW(STCP); OUTPUT_HIGH(STCP);
+}
+

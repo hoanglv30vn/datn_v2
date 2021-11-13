9e69539cf0a888ad34c91d41161ffe7348ee8a69
hoanglv <hoanglv.30@gmail.com>
2021-11-13 15:50:09 +0700

    fix uart rcv esp. (clear data. --> nhận data)

--- ESP/test.c/test.c.ino
+++ ESP/test.c/test.c.ino
@@ -16,7 +16,7 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 int button = 12; //GPIO12 -- D6  
 int buttonState = 0;
 int count = 0;
-char *count_char ="";
+char *data_uart_char ="";
 char uart_data_rcv[50];
 int tt_rcv_uart = 0;
 int data = 0;
@@ -26,25 +26,27 @@ int count1=0;
 //----------------Hàm--------------//
 
 /***********RESET_ESP XOA_WIFI***********/
-void reset_erase_wifi(){
+void reset_erase_wifi()
+{
   WiFiManager wifiManager;
   wifiManager.erase();
   ESP.reset();
-  }
+}
 void checkbutt(){
-  for( int i=0; i<20; i++){
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(75);
-      buttonState=digitalRead(button);       
-      if (!buttonState){ return; }      
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(75);          
-    }
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(750);
+  for( int i=0; i<20; i++)
+  {
     digitalWrite(LED_BUILTIN, LOW);
-    reset_erase_wifi();
+    delay(75);
+    buttonState=digitalRead(button);       
+    if (!buttonState){ return; }      
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(75);          
   }
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(750);
+  digitalWrite(LED_BUILTIN, LOW);
+  reset_erase_wifi();
+ }
 /***********WIFI MANAGER***********/
 void configModeCallback (WiFiManager *myWiFiManager)
 {
@@ -52,8 +54,7 @@ void configModeCallback (WiFiManager *myWiFiManager)
   s.println(WiFi.softAPIP());
   s.println(myWiFiManager->getConfigPortalSSID());
 }
-void connect_config_wifi(){
-    
+void connect_config_wifi(){    
   //Khai báo wifiManager thuộc class WiFiManager, được quy định trong file WiFiManager.h
   WiFiManager wifiManager;
   //có thểreset các cài đặt cũ bằng cách gọi hàm:
@@ -82,90 +83,92 @@ void connect_config_wifi(){
   s.println(port);    
   }
 /***********UART***********/
-  int read_uart_int(){
-    int index = 1;
-    while(tt_rcv_uart){    
-       data = s.read();
-       if (data < 0)   {        
+  int read_uart_char(){
+    int index = 0;
+    tt_rcv_uart = 1;
+    data = s.read();
+    if (data > 0)   
+    {                                 
+      for (int i = 1; i<50; i++)
+      {
+        uart_data_rcv[i]='\0';
+      } 
+      uart_data_rcv[0]=data;  
+      while(tt_rcv_uart)
+      {    
+        data = s.read();
+        if (data < 0)
+        {                            
           tt_rcv_uart = 0;
         }
-        else{
-          uart_data_rcv[index]=data;
-          index++; 
+        else 
+        {
+          index++;
+          uart_data_rcv[index]=data;           
         }
-      }    
-      return  index;
-    }
-  int read_uart(){
-      int index = 0;  
-      tt_rcv_uart = 0;  
-      data = s.read();   
-      if (data >0)
-      {       
-        uart_data_rcv[0]=data;
-        tt_rcv_uart = 1;
-        index = read_uart_int();  
-      }              
-    return  index;
-  }  
-/***********TCP***********/  
-void client_wifi(){
-    WiFiClient client = server.available();  
-     if (client) {
-    if(client.connected())
-    {
-      s.println("Client Connected");
-    }
-    
-    while(client.connected()){      
-      while(client.available()>0){
-        // read data from the connected client
-        s.write(client.read()); 
-      }
-      //Send Data to connected client
-      while(s.available()>0)
-      {
-        client.write(s.read());
-      }
-    }
-    client.stop();
-    s.println("Client disconnected");    
+      }                  
+    }        
+  return  index;
   }
+/***********TCP***********/  
+void client_wifi()
+{
+    WiFiClient client = server.available();  
+    if (client) 
+    {
+      if(client.connected())
+      {
+        s.println("Client Connected");
+      }    
+      while(client.connected())
+      {      
+        while(client.available()>0)
+        {
+          // read data from the connected client
+          s.write(client.read()); 
+        }
+        //Send Data to connected client
+        while(s.available()>0)
+        {
+          client.write(s.read());
+        }
+      }
+      client.stop();
+      s.println("Client disconnected");    
+    }
 }
 void setup() {
   //Serial S Begin at 9600 Baud  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   s.begin(9600);
   connect_config_wifi();
   WiFiServer server(port);  
-  pinMode(LED_BUILTIN, OUTPUT);
+  
   pinMode(button, INPUT);
   lcd.begin();
   lcd.backlight();
   lcd.print("Heyyy u");
-
 }
 
 void loop() {  
+  count++;  
   client_wifi();
   digitalWrite(LED_BUILTIN, HIGH);
-  len_data = read_uart();
-  itoa(count, count_char, 10);
+  len_data = read_uart_char();
   /*butt reset wifi, reset esp*/
-  buttonState=digitalRead(button);
+  buttonState=digitalRead(button);  
   if (buttonState){
     checkbutt();
     }
-    
-  s.write(count_char);
-  s.write("\n");
   lcd.setCursor(0,1);
   lcd.print(uart_data_rcv); 
   lcd.setCursor(14,0);
   lcd.print(len_data);        
   lcd.setCursor(8,0);
   lcd.print(count);    
-  //s.write(data);
-  count++;
-  delay(500);
+  delay(1000);  
+  lcd.setCursor(0,1);
+  lcd.print("                 "); 
   delay(500);
 }
